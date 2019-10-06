088646154612d0ebe4d51a209f6c9bbdbb4d895d
Andrius <and.bankauskas@gmail.com>
2019-10-06 14:03:59 +0200

    GameState management

---
+++ src/main/game/GameState.java
@@ -0,0 +1,90 @@
+package game;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class GameState {
+    public int[] scores = { 0, 0 };
+    public byte[] board = { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 };
+    public int moveOfPlayer = 0;
+
+    private boolean over = false;
+
+    public void play(int field){
+        field = field - 1;
+        if(!validMove(field)){
+            throw new RuntimeException("Move: " + field + " is not valid for player " + moveOfPlayer);
+        }
+        int startField = field;
+        int value = board[field];
+        board[field] = 0;
+        while(value > 0) {
+            field = (++field) % 12;
+            board[field]++;
+            value--;
+        }
+
+        if(board[field] == 2 || board[field] == 4 || board[field] == 6) {
+            do {
+                scores[startField/6] += board[field];
+                board[field] = 0;
+                field = (field == 0) ? field = 11 : --field;
+            } while(board[field] == 2 || board[field] == 4 || board[field] == 6);
+        }
+        moveOfPlayer = moveOfPlayer == 0 ? 1 : 0;
+        if(isGameOver()){
+            over = true;
+            int winner = moveOfPlayer == 0 ? 1 : 0;
+            for(int i = 0; i < 6; i++){
+                scores[winner] += this.board[i+6*winner];
+            }
+        }
+    }
+
+    private boolean validMove(int field){
+        return field >= 6*moveOfPlayer && field <= 6*moveOfPlayer + 6;
+    }
+
+    public GameState copy(){
+        GameState gs = new GameState();
+        gs.board = this.board.clone();
+        gs.scores = this.scores.clone();
+        gs.moveOfPlayer = this.moveOfPlayer;
+        return gs;
+    }
+
+    public boolean isGameOver(){
+        if(this.over) return true;
+        boolean canMove = false;
+        for(int i = 0; i < 6; i++){
+            canMove |= this.board[i+6*moveOfPlayer] > 0;
+        }
+        return !canMove;
+    }
+
+    @Override
+    public String toString(){
+        String fmt = "%2d";
+        StringBuilder sb = new StringBuilder();
+        String header = IntStream.range(0,27).mapToObj(s -> "=").collect(Collectors.joining(""));
+        sb.append(header);
+        sb.append("\n");
+        sb.append("p1 | ");
+        for(int i = 11; i >= 6; i--){
+            sb.append(String.format(fmt, this.board[i]));
+            sb.append(" ");
+        }
+        sb.append("| p2\n");
+        sb.append(String.format(fmt, this.scores[0]));
+        sb.append(" | ");
+        for(int i = 0; i < 6; i++){
+            sb.append(String.format(fmt, this.board[i]));
+            sb.append(" ");
+        }
+        sb.append("| ");
+        sb.append(String.format(fmt, this.scores[1]));
+        sb.append("\n");
+        sb.append(header);
+        return sb.toString();
+    }
+}
---
+++ src/test/GameStateTests.java
@@ -0,0 +1,76 @@
+import game.GameState;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class GameStateTests{
+
+    @Test
+    public void prettyStringTest(){
+        GameState gs = new GameState();
+        assertEquals(
+                "===========================\n" +
+                "p1 |  6  6  6  6  6  6 | p2\n" +
+                " 0 |  6  6  6  6  6  6 |  0\n" +
+                "===========================",
+                gs.toString());
+    }
+
+    @Test
+    public void validMoveTest(){
+        GameState gs = new GameState();
+        gs.play(0);
+        gs.play(11);
+        assertArrayEquals(new byte[]{ 1, 8, 8, 8, 8, 8, 7, 6, 6, 6, 6, 0 }, gs.board);
+    }
+
+    @Test
+    public void pointCollection(){
+        GameState gs = new GameState();
+        gs.board = new byte[] { 5, 3, 3, 5, 3, 3, 7, 6, 6, 6, 6, 0 };
+        gs.play(0);
+        assertEquals(22, gs.scores[0]);
+    }
+
+    @Test
+    public void crossTeamPointCollection(){
+        GameState gs = new GameState();
+        gs.board = new byte[] { 5, 3, 3, 5, 3, 3, 5, 3, 3, 5, 3, 3 };
+        gs.play(0);
+        gs.play(6);
+        assertEquals(22, gs.scores[0]);
+        assertEquals(22, gs.scores[1]);
+    }
+
+    @Test
+    public void crossTeamBoundaryPointCollection(){
+        GameState gs = new GameState();
+        gs.board = new byte[] { 3, 3, 1, 1, 3, 3, 5, 5, 3, 5, 3, 5 };
+        gs.play(2);
+        gs.play(10);
+        assertEquals(2, gs.scores[0]);
+        assertEquals(14, gs.scores[1]);
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void invalidMoveDetection(){
+        expectedException.expect(RuntimeException.class);
+        GameState gs = new GameState();
+        gs.play(0);
+        gs.play(1);
+    }
+
+    @Test
+    public void invalidMoveDetectionP2(){
+        expectedException.expect(RuntimeException.class);
+        GameState gs = new GameState();
+        gs.play(0);
+        gs.play(10);
+        gs.play(10);
+    }
+}
