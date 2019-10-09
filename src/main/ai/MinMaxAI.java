9ecd2029f5cf5ebfde3684a47f51993718b506e8
Andrius-B <and.bankauskas@gmail.com>
2019-10-09 15:20:52 +0200

    MinMax pseudocode

--- src/main/ai/MinMaxAI.java
+++ src/main/ai/MinMaxAI.java
@@ -22,8 +22,53 @@ public class MinMaxAI extends AI
             playingAs = 1;
         }
 
-        
+
 
         return 0;
+    }
+
+    private int minimax(int depth, int nodeIndex,
+                        Boolean maximizingPlayer,
+                        int values[], int alpha,
+                        int beta) {
+        // Terminating condition. i.e
+        // leaf node is reached
+        if (depth == 3)
+            return values[nodeIndex];
+
+        if (maximizingPlayer) {
+            int best = Integer.MIN_VALUE;
+
+            // Recur for left and
+            // right children
+            for (int i = 0; i < 2; i++) {
+                int val = minimax(depth + 1, nodeIndex * 2 + i,
+                        false, values, alpha, beta);
+                best = Math.max(best, val);
+                alpha = Math.max(alpha, best);
+
+                // Alpha Beta Pruning
+                if (beta <= alpha)
+                    break;
+            }
+            return best;
+        } else {
+            int best = Integer.MAX_VALUE;
+
+            // Recur for left and
+            // right children
+            for (int i = 0; i < 2; i++) {
+
+                int val = minimax(depth + 1, nodeIndex * 2 + i,
+                        true, values, alpha, beta);
+                best = Math.min(best, val);
+                beta = Math.min(beta, best);
+
+                // Alpha Beta Pruning
+                if (beta <= alpha)
+                    break;
+            }
+            return best;
+        }
     }
 }
