001f7e8e742b45dbd8285bedfc97e2ad5f715776
LordOtut <pascal99.schmidt@googlemail.com>
2019-10-09 14:35:32 +0200

    abstract node

---
+++ src/main/ai/AbstractNode.java
@@ -0,0 +1,52 @@
+package ai;
+
+import game.GameState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractNode {
+  private GameState state;
+  private AbstractNode parent;
+  private List<AbstractNode> children = null;
+
+  public AbstractNode(AbstractNode parent, GameState gs){
+    this.state = gs;
+    this.parent = parent;
+  }
+
+  public List<AbstractNode> getChildren() {
+    // lazy initialization of children
+    if(children == null){
+      this.children = new ArrayList<>();
+      /*this.state.getValidMoves().forEach(move -> {
+        GameState gs = this.state.copy();
+        gs.play(move);
+        Node n = new Node(this, gs);
+        this.children.add(n);
+      });*/
+    }
+    return children;
+  }
+
+  public void setChildren(List<AbstractNode> children){
+    this.children = children;
+  }
+
+  public GameState getState() {
+    return state;
+  }
+
+  public AbstractNode getParent() {
+    return parent;
+  }
+
+  public void setParent(AbstractNode parent) {
+    this.parent = parent;
+  }
+
+  public boolean isLeaf(){
+    return children == null;
+  }
+
+}
